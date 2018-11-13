697ba8788502d2e452ad1d0a02383dcfa78ccfb3
xNOtme <tdhkcc7@gmail.com>
2018-11-14 01:23:20 +0700

    Add Project

---
+++ QuanLyCuaHang/UserControlQLCH/ControlBarUI.xaml.cs
@@ -0,0 +1,31 @@
+﻿using QuanLyCuaHang.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyCuaHang.UserControlQLCH
+{
+    /// <summary>
+    /// Interaction logic for ControlBarUI.xaml
+    /// </summary>
+    public partial class ControlBarUI : UserControl
+    {
+        public ControlBarViewModel ViewModel { get; set; }
+        public ControlBarUI()
+        {
+            InitializeComponent();
+            this.DataContext = ViewModel = new ControlBarViewModel();
+        }
+    }
+}
---
+++ QuanLyCuaHang/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace QuanLyCuaHang.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool Isloaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                Isloaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+            );
+        }
+    }
+}
