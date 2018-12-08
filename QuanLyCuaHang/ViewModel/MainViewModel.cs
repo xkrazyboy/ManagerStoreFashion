057097408415b1f99ccc1d2b3a0f50a4dac09236
xNOtme <tdhkcc7@gmail.com>
2018-12-08 15:53:31 +0700

    Unit Customer and User

--- QuanLyCuaHang/ViewModel/MainViewModel.cs
+++ QuanLyCuaHang/ViewModel/MainViewModel.cs
@@ -14,6 +14,9 @@ namespace QuanLyCuaHang.ViewModel
         public ICommand LoadedWindowCommand { get; set; }
         public ICommand UnitCommand { get; set; }
         public ICommand SuplierCommand { get; set; }
+        public ICommand CustomerCommand { get; set; }
+        public ICommand ObjectCommand { get; set; }
+        public ICommand UserCommand { get; set; }
 
         public MainViewModel()
         {
@@ -27,6 +30,9 @@ namespace QuanLyCuaHang.ViewModel
 
             UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) => { UnitWindow wd = new UnitWindow(); wd.ShowDialog(); });
             SuplierCommand = new RelayCommand<object>((p) => { return true; }, (p) => { SuplierWindow wd = new SuplierWindow(); wd.ShowDialog(); });
+            CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) => { CustomerWindow wd = new CustomerWindow(); wd.ShowDialog(); });
+            ObjectCommand = new RelayCommand<object>((p) => { return true; }, (p) => { ObjectWindow wd = new ObjectWindow(); wd.ShowDialog(); });
+            UserCommand = new RelayCommand<object>((p) => { return true; }, (p) => { UserWindow wd = new UserWindow(); wd.ShowDialog(); });
         }
     }
 }
