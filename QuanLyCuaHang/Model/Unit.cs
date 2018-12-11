ae1440b89872847e8f0e8cad357eef2bbfd03422
xNOtme <tdhkcc7@gmail.com>
2018-12-12 02:34:29 +0700

    Thêm sửa Unit & Suplier

--- QuanLyCuaHang/Model/Unit.cs
+++ QuanLyCuaHang/Model/Unit.cs
@@ -9,10 +9,11 @@
 
 namespace QuanLyCuaHang.Model
 {
+    using QuanLyCuaHang.ViewModel;
     using System;
     using System.Collections.Generic;
     
-    public partial class Unit
+    public partial class Unit : BaseViewModel
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Unit()
